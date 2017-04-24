1295b23c1a7f1cb1799b4fc5be6ae58cafc046f5
penserbjorne <ps_3_xxx@hotmail.com>
2017-04-23 22:20:24 -0500

    Se termino el programa 2, no ha sido probado :(

--- Practicas_Teoria/practica2.asm
+++ Practicas_Teoria/practica2.asm
@@ -7,6 +7,7 @@ include		<p16f877A.inc>
 nL		equ	H'30'
 nH		equ	H'31'
 reg1 	equ H'32'
+codigo	equ H'69'
 
     org 0
     goto inicio
@@ -24,20 +25,28 @@ inicio:
     movwf   ADCON0		;	Configuracion del ADCON0
 	;	Configuracion propia
 	movlw	H'FF'		;	W = FF
-	movwf	reg1			;	reg1 = W
+	movwf	reg1		;	reg1 = W
+	movlw	H'FF'		;	Codigo asignado por el profesor, le agregamos un valor mas, era FE
+	movwf	codigo		;	codigo = W	
     
 	;	Conversion
 convertir:
 	bsf		ADCON0, 2	;	Pon 1 en el bit 2 de ADCON2 [Ordena conversion]
-    ; preguntamos si terminó
+    ;	Preguntamos si terminó
 preguntar:
     btfsc	ADCON0, 2	;	Revisa el bit 2, si es 0 salta
     goto    preguntar	;	Volvemos a preguntar
-desplegar:
     movf    ADRESH, W	;	W = ADRESH
-    movwf   PORTB		;	PORTB = W
 conversion:
-	movf	ADRESL,W		;	Carga lo que tenga resL en W
+	;	Verificamos el tope superior
+	;	W es igual a ADRESH
+	subwf	codigo,W	;	W = W - codigo
+ 	btfsc 	STATUS,C	;	Verificamos la bandera de carry
+    goto	convertir	;	ADRESH >= codigo
+
+	;	Si no hemos llegado al tope superior
+	;	Convertimos
+	movf	ADRESL,W	;	Carga lo que tenga resL en W
 	andlw 	H'0F'		;	Enmascaramiento para el nemles low
 	movwf 	nL			;	nL = W [nmles low]
 	movf	ADRESH,W		;	Carga lo que tenga resH en W
@@ -81,7 +90,6 @@ Tabla:
 	retlw	H'AD'
 	retlw	H'AE'
 	retlw	H'AF'
-	return
 
 	;	Retardo
 retardo:
