b70eef78e98564197487024b2df464275a0b5ed6
Emilio Cabrera <emilio1625@gmail.com>
2017-04-28 00:06:49 -0500

    Se corrigen los comentarios Se añade licencia al archivo Se elimina variable no necesaria Se corrigen algunas lineas

--- Practicas_Teoria/practica3/p3.asm
+++ Practicas_Teoria/practica3/p3.asm
@@ -1,10 +1,33 @@
+    ; Copyright (c) 2017 Emilio Cabrera
+    ;
+    ; MIT License
+    ;
+    ; Permission is hereby granted, free of charge, to any person obtaining
+    ; a copy of this software and associated documentation files (the
+    ; "Software"), to deal in the Software without restriction, including
+    ; without limitation the rights to use, copy, modify, merge, publish,
+    ; distribute, sublicense, and/or sell copies of the Software, and to
+    ; permit persons to whom the Software is furnished to do so, subject to
+    ; the following conditions:
+    ;
+    ; The above copyright notice and this permission notice shall be
+    ; included in all copies or substantial portions of the Software.
+    ;
+    ; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    ; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    ; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    ; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+    ; LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+    ; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    ; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    ;
+
     processor 16f877a
     include <p16f877a.inc>
 
 str     EQU 0x20; direccion del registro a mostrar
 off     EQU 0x21;
 toogle  EQU 0x22;
-conta   EQU 0x23;
 temp    EQU 0x7b;
 index0  EQU 0x7c; contador
 index1  EQU 0x7d;
@@ -17,28 +40,26 @@ index3  EQU 0x7f;
 
 setup:
     bcf     STATUS, RP1
-    bsf     STATUS, RP0
+    bsf     STATUS, RP0 ; cambiamos al banco 1
     movlw   0x07
-    movwf   ADCON1
-    movwf   CMCON
-    movlw   0xff
-    movwf   TRISA       ; porta input
-    clrf    TRISB       ; portb output
-    movlw   0x3f
-    movwf   TRISD       ; pd7, pd6 out
-    bcf     STATUS, RP0
-    clrf    PORTA
-    clrf    PORTB
-    clrf    PORTD
-    bsf     PORTD, 6    ; encendemos primero display derecho
-    movlw   0x0F
-    movwf   index1      ; retraso inicial
+    movwf   ADCON1      ; desactivamos el ADC
+    movwf   CMCON       ; desactivamos comparadores
+    movlw   0xff        ;
+    movwf   TRISA       ; PORTA como entrada
+    clrf    TRISB       ; PORTB como salida
+    movlw   0x3f        ; 0b11000000
+    movwf   TRISD       ; bits 6 y siete de PORTD como salida
+    bcf     STATUS, RP0 ; regresamos al banco 0
+    clrf    PORTA       ;
+    clrf    PORTB       ;
+    clrf    PORTD       ; limpiamos latches de los puertos
+    bsf     PORTD,  6   ; encendemos primero display derecho
+    movlw   0x0f
+    movwf   index1      ; retraso asinado por el profe (3 segundos)
     movlw   0xfe        ; cadena inicial
-    movwf   str
-    clrf    off         ; off = false
-    clrf    toogle      ; toogle = false
-    movlw   0x0f        ; si es cero, reinicia el retraso largo
-    movwf   conta       ; reinicia el retraso
+    movwf   str         ; la guardamos en el registro
+    clrf    off         ; indica si hay que apagar los displays, off = false
+    clrf    toogle      ; indica si hay que parpadear los displays, toogle=false
 
 inicio:
     movlw   0x09
@@ -58,22 +79,24 @@ dig1:                   ; mostramos nuestra clave
 dig2:
     movlw   0x0e        ; primer digito de nuestro codigo
     call    put         ; imprime el valor
-    bsf     PORTB,  0   ;
+    bsf     PORTB,  0   ; encendemos el punto decimal
     call    delay       ; retraso
     movlw   0x0f        ; digito mas significativo de nuestro codigo
     call    put         ; mostramos en display
-    bsf     PORTB,  0   ;
+    bsf     PORTB,  0   ; encendemos el punto decimal
     call    delay       ; retraso
     decfsz  index0, F   ; decrementamos el contador
-    goto    dig2        ; si no es cero y volvemos al principio
+    goto    dig2        ; si no es cero, volvemos al principio
                         ; si es cero continuamos
     decfsz  index1, F   ; decrementamos el contador
     goto    inicio      ; si no es cero y volvemos al principio
                         ; si no continuamos
 
+    movlw   0x0f        ;
+    movwf   index1      ; restauramos el valor del retraso
 
 main:
-    movf    PORTA,  W   ; recuperamos el valor de porta
+    movf    PORTA,  W   ; recuperamos el valor de PORTA
     movwf   temp        ; y lo guadamos temporalmente
     ; switch
     sublw   0x2e        ;
@@ -97,7 +120,7 @@ main:
     goto    boton5      ;
     movf    temp,   W   ;
 disp:
-    movlw   0x12        ; 31 veces
+    movlw   0x12        ; 18 veces
     movwf   index0      ; retraso
 loop:
     movf    str,    W   ; cargamos el valor a mostrar
@@ -106,41 +129,45 @@ loop:
     swapf   str,    W   ; intercambiamos nibles y guardamos en w
     call    put         ; mostramos en los diplays
     call    delay       ; y esperamos
-    decfsz  index0      ; retraso de 300ms aprox antes de leer el siguiente clic
+    decfsz  index0      ; retraso antes de leer el siguiente clic
     goto    loop        ; si index0 no es 0 muestra el valor
 
-    movlw   0xff        ;
-    btfsc   toogle, 0   ; si no estamos parpadeando
-    decfsz  conta       ; si parpadeamos decrementa contador y si no es cero
+
+    btfsc   toogle, 0   ; si no estamos parpadeando ignora la siguiente instrucion
+    decfsz  conta       ; si parpadeamos decrementa contador y si no es cero..
     goto    main        ; regresamos a checar el boton
-    movlw   0x0f        ; si es cero, reinicia el retraso largo
-    movwf   conta       ; reinicia el retraso
-    xorwf   off,    F   ; toogle off
+    movlw   0x0f        ; .. si es cero, reinicia el retraso largo
+    movwf   index1      ; reinicia el retraso
+    xorwf   off,    F   ; switchea la bandera para apagar los displays
     goto    main        ; regresamos a checar el boton
+
 
 boton1:
     swapf   str,    F   ; intecambiamos los nibbles del codigo a mostrar
     goto    disp        ; regresamos a mostrar el valor
+
 boton2:
     movlw   0x00        ; ceros
     movwf   str         ; ponemos a ceros el valor a mostrar
     goto    disp        ; regresamos a mostrar el valor
+
 boton3:
     movlw   0xff        ; mascara
     xorwf   toogle, F   ; parpadear o no parpadear
     btfss   toogle, 7   ; si no parpadeamos
     clrf    off         ; encendemos los leds
     goto    disp        ; regresamos a mostrar el valor
+
 boton4:
     incf    str,    F   ; incrementamos el valor a mostrar
     goto    disp        ; regresamos a mostrar el valor
+
 boton5:
     decf    str,    F   ; decrementamos el valor a mostrar
     goto    disp        ; regresamos a mostrar el valor
 
 
 ; muestra en los display el contenido de w
-; debe ser llamado primero con el valor mas
 put:
     andlw   0x0f        ; nos deshacemos del nibble mas significativo
     movwf   temp        ; guarda el valor de w temporalmente
@@ -153,46 +180,47 @@ put:
     movwf   PORTB       ; lo mostramos en portb
     return
 
-
+; obtiene el codigo de 7 segmentos del valor en el acumulador
 getseg:
-    addwf	PCL,    F   ; w debe tener el valor a mostrar
-            ; abcdefgh
-    retlw	b'11111100'	; el codigo 7 segmentos para el 0
-    retlw	b'01100000'	; el codigo 7 segmentos para el 1
-    retlw	b'11011010'	; el codigo 7 segmentos para el 2
-    retlw	b'11110010'	; el codigo 7 segmentos para el 3
-    retlw	b'01100110'	; el codigo 7 segmentos para el 4
-    retlw	b'10110110'	; el codigo 7 segmentos para el 5
-    retlw	b'10111110'	; el codigo 7 segmentos para el 6
-    retlw	b'11100000'	; el codigo 7 segmentos para el 7
-    retlw	b'11111110'	; el codigo 7 segmentos para el 8
-    retlw	b'11100110'	; el codigo 7 segmentos para el 9
-    retlw	b'11101110'	; el codigo 7 segmentos para el a
-    retlw	b'00111110'	; el codigo 7 segmentos para el b
-    retlw	b'10011100'	; el codigo 7 segmentos para el c
-    retlw	b'01111010'	; el codigo 7 segmentos para el d
-    retlw	b'10011110'	; el codigo 7 segmentos para el e
-    retlw	b'10001110'	; el codigo 7 segmentos para el f
+    addwf  PCL,    F   ; w debe tener el valor a mostrar
+          ;  abcdefgh
+    retlw  b'11111100'  ; el codigo 7 segmentos para el 0
+    retlw  b'01100000'  ; el codigo 7 segmentos para el 1
+    retlw  b'11011010'  ; el codigo 7 segmentos para el 2
+    retlw  b'11110010'  ; el codigo 7 segmentos para el 3
+    retlw  b'01100110'  ; el codigo 7 segmentos para el 4
+    retlw  b'10110110'  ; el codigo 7 segmentos para el 5
+    retlw  b'10111110'  ; el codigo 7 segmentos para el 6
+    retlw  b'11100000'  ; el codigo 7 segmentos para el 7
+    retlw  b'11111110'  ; el codigo 7 segmentos para el 8
+    retlw  b'11100110'  ; el codigo 7 segmentos para el 9
+    retlw  b'11101110'  ; el codigo 7 segmentos para el a
+    retlw  b'00111110'  ; el codigo 7 segmentos para el b
+    retlw  b'10011100'  ; el codigo 7 segmentos para el c
+    retlw  b'01111010'  ; el codigo 7 segmentos para el d
+    retlw  b'10011110'  ; el codigo 7 segmentos para el e
+    retlw  b'10001110'  ; el codigo 7 segmentos para el f
 
+; rutina de retardo
 ; delay = 0.05 seconds
-; clock frequency = 20 mhz
+; clock frequency = 20 MHz
 ; actual delay = 0.005 seconds = 25000 cycles
 ; error = 0 %
 delay:
-			;24993 cycles
-	movlw	0x86
-	movwf	index2
-	movlw	0x14
-	movwf	index3
+        ;24993 cycles
+    movlw  0x86
+    movwf  index2
+    movlw  0x14
+    movwf  index3
 loopd:
-	decfsz	index2, f
-	goto	$+2
-	decfsz	index3, f
-	goto	loopd
-			;3 cycles
-	goto	$+1
-	nop
-			;4 cycles (including call)
-	return
+    decfsz  index2, F
+    goto  $+2
+    decfsz  index3, F
+    goto  loopd
+        ;3 cycles
+    goto  $+1
+    nop
+        ;4 cycles (including call)
+    return
 
     end
